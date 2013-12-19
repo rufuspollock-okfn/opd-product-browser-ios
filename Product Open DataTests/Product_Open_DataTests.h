cfda860c759d711a8e3e1325929b50a982a5d302
Fabrice-Rimbault <Fabrice@iMac-de-Fabrice.local>
2013-12-19 18:27:32 +0100

    first version - just interface

---
+++ Product Open DataTests/Product_Open_DataTests.h
@@ -0,0 +1,13 @@
+//
+//  Product_Open_DataTests.h
+//  Product Open DataTests
+//
+//  Created by Fabrice on 17/12/13.
+//  Copyright (c) 2013 Open Knowledge Foundation. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Product_Open_DataTests : SenTestCase
+
+@end
---
+++ Product Open DataTests/Product_Open_DataTests.m
@@ -0,0 +1,32 @@
+//
+//  Product_Open_DataTests.m
+//  Product Open DataTests
+//
+//  Created by Fabrice on 17/12/13.
+//  Copyright (c) 2013 Open Knowledge Foundation. All rights reserved.
+//
+
+#import "Product_Open_DataTests.h"
+
+@implementation Product_Open_DataTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Product Open DataTests");
+}
+
+@end
